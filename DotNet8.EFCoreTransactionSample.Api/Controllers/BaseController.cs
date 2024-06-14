7f235de5f9f8df312f27d0dc793fe106a91b6a02
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-15 00:28:34 +0630

    -

--- DotNet8.EFCoreTransactionSample.Api/Controllers/BaseController.cs
+++ DotNet8.EFCoreTransactionSample.Api/Controllers/BaseController.cs
@@ -1,21 +1,19 @@
 ﻿using DotNet8.EFCoreTransactionSample.Api.Resources;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.EFCoreTransactionSample.Api.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
-    {
-        protected IActionResult Created()
-        {
-            return StatusCode(201, MessageResource.SavingSuccessful);
-        }
+namespace DotNet8.EFCoreTransactionSample.Api.Controllers;
 
-        protected IActionResult HandleFailure(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected IActionResult Created()
+    {
+        return StatusCode(201, MessageResource.SavingSuccessful);
+    }
+
+    protected IActionResult HandleFailure(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
