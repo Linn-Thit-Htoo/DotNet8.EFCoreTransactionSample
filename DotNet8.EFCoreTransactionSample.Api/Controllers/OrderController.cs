cdaba8dec59d8c5386ee34e7ae9b4aefdd4b9d15
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-15 00:29:02 +0630

    -

--- DotNet8.EFCoreTransactionSample.Api/Controllers/OrderController.cs
+++ DotNet8.EFCoreTransactionSample.Api/Controllers/OrderController.cs
@@ -4,66 +4,65 @@ using DotNet8.EFCoreTransactionSample.Mapper;
 using DotNet8.EFCoreTransactionSample.Models.Setup.Order;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNet8.EFCoreTransactionSample.Api.Controllers
+namespace DotNet8.EFCoreTransactionSample.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController : BaseController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class OrderController : BaseController
+    private readonly AppDbContext _appDbContext;
+
+    public OrderController(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
+        _appDbContext = appDbContext;
+    }
 
-        public OrderController(AppDbContext appDbContext)
+    [HttpPost]
+    public async Task<IActionResult> SubmitOrder([FromBody] OrderRequestModel requestModel)
+    {
+        var transaction = await _appDbContext.Database.BeginTransactionAsync();
+        try
         {
-            _appDbContext = appDbContext;
-        }
+            if (string.IsNullOrEmpty(requestModel.CustomerCode))
+                return BadRequest();
 
-        [HttpPost]
-        public async Task<IActionResult> SubmitOrder([FromBody] OrderRequestModel requestModel)
-        {
-            var transaction = await _appDbContext.Database.BeginTransactionAsync();
-            try
-            {
-                if (string.IsNullOrEmpty(requestModel.CustomerCode))
-                    return BadRequest();
+            if (requestModel.TotalAmount <= 0)
+                return BadRequest();
 
-                if (requestModel.TotalAmount <= 0)
-                    return BadRequest();
+            if (requestModel.OrderDetails is null)
+                return BadRequest();
 
-                if (requestModel.OrderDetails is null)
-                    return BadRequest();
+            var orderModel = requestModel.Change();
+            await _appDbContext.TblOrders.AddAsync(orderModel);
+            int orderResult = await _appDbContext.SaveChangesAsync();
 
-                var orderModel = requestModel.Change();
-                await _appDbContext.TblOrders.AddAsync(orderModel);
-                int orderResult = await _appDbContext.SaveChangesAsync();
-
-                var orderDetailModel = requestModel
-                    .OrderDetails.Select(x => new TblOrderDetail()
-                    {
-                        OrderDetailId = Guid.NewGuid().ToString(),
-                        OrderId = orderModel.OrderId,
-                        PricePerItem = x.PricePerItem,
-                        ProductCode = x.ProductCode,
-                        Quantity = x.Quantity,
-                    })
-                    .ToList();
-
-                await _appDbContext.TblOrderDetails.AddRangeAsync(orderDetailModel);
-                int result = await _appDbContext.SaveChangesAsync();
-
-                if (orderResult > 0 && result > 0)
+            var orderDetailModel = requestModel
+                .OrderDetails.Select(x => new TblOrderDetail()
                 {
-                    await transaction.CommitAsync();
-                    return Created();
-                }
+                    OrderDetailId = Guid.NewGuid().ToString(),
+                    OrderId = orderModel.OrderId,
+                    PricePerItem = x.PricePerItem,
+                    ProductCode = x.ProductCode,
+                    Quantity = x.Quantity,
+                })
+                .ToList();
 
-                await transaction.RollbackAsync();
-                return BadRequest(MessageResource.SavingFail);
-            }
-            catch (Exception ex)
+            await _appDbContext.TblOrderDetails.AddRangeAsync(orderDetailModel);
+            int result = await _appDbContext.SaveChangesAsync();
+
+            if (orderResult > 0 && result > 0)
             {
-                await transaction.RollbackAsync();
-                return HandleFailure(ex);
+                await transaction.CommitAsync();
+                return Created();
             }
+
+            await transaction.RollbackAsync();
+            return BadRequest(MessageResource.SavingFail);
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+            return HandleFailure(ex);
         }
     }
 }
