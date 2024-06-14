19f7acc9dc3b72241492637962b00d0bd28e5974
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-15 00:30:25 +0630

    -

--- DotNet8.EFCoreTransactionSample.Models/Setup/Order/OrderModel.cs
+++ DotNet8.EFCoreTransactionSample.Models/Setup/Order/OrderModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNet8.EFCoreTransactionSample.Models.Setup.Order
+﻿namespace DotNet8.EFCoreTransactionSample.Models.Setup.Order
 {
     public class OrderModel
     {
