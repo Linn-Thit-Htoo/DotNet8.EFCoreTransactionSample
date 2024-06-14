aa1df5f67ec3bae01b2661720a9953745847fed5
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-15 00:30:02 +0630

    -

--- DotNet8.EFCoreTransactionSample.Models/Setup/Order/OrderDetailModel.cs
+++ DotNet8.EFCoreTransactionSample.Models/Setup/Order/OrderDetailModel.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNet8.EFCoreTransactionSample.Models.Setup.Order
+﻿namespace DotNet8.EFCoreTransactionSample.Models.Setup.Order;
+
+public class OrderDetailModel
 {
-    public class OrderDetailModel
-    {
-        public string OrderDetailId { get; set; } = null!;
+    public string OrderDetailId { get; set; } = null!;
 
-        public string OrderId { get; set; } = null!;
+    public string OrderId { get; set; } = null!;
 
-        public string ProductCode { get; set; } = null!;
+    public string ProductCode { get; set; } = null!;
 
-        public int Quantity { get; set; }
+    public int Quantity { get; set; }
 
-        public decimal PricePerItem { get; set; }
-    }
+    public decimal PricePerItem { get; set; }
 }
